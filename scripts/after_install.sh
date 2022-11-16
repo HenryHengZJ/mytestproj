dcc4f021c22a53a37021afccb54c4207f9b81f57
Henry <hzj94@hotmail.com>
2022-11-16 13:06:20 +0000

    initial

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-sudo apt-get update
-
 curl -sL https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh -o install_nvm.sh
 bash install_nvm.sh
 export NVM_DIR="$HOME/.nvm"
@@ -10,8 +8,9 @@ export NVM_DIR="$HOME/.nvm"
 nvm install --lts
 nvm --version
 
-sudo apt-get install nodejs
-sudo apt-get install npm
+sudo yum install -y gcc-c++ make 
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
+sudo yum install -y nodejs
 
 npm install --location=global yarn
 yarn global add pm2
