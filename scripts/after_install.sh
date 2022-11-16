989177e1ca4c7818ea87cef3dd826fb5a879215d
Henry <hzj94@hotmail.com>
2022-11-16 12:59:03 +0000

    initial

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+sudo apt-get update
 curl -sL https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh -o install_nvm.sh
 bash install_nvm.sh
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+. ~/.nvm/nvm.sh
 nvm install --lts
+nvm --version
+node -v
+
 npm install --location=global yarn
 yarn global add pm2
 
