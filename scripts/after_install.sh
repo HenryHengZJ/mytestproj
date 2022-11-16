eda15cc0429cf8c73e2eeb1fb4477ef0b7b674c8
Henry <hzj94@hotmail.com>
2022-11-16 12:43:21 +0000

    initial

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-echo 'run after_install.sh: ' >> /home/ec2-user/mytestproj/deploy.log
+curl -sL https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh -o install_nvm.sh
+bash install_nvm.sh
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install --lts
+npm install --location=global yarn
+yarn global add pm2
 
-echo 'cd /home/ec2-user/mytestproj' >> /home/ec2-user/mytestproj/deploy.log
-cd /home/ec2-user/mytestproj >> /home/ec2-user/mytestproj/deploy.log
-
-echo 'yarn install' >> /home/ec2-user/mytestproj/deploy.log 
-yarn install >> /home/ec2-user/mytestproj/deploy.log
+cd /home/ec2-user/mytestproj
+yarn install
